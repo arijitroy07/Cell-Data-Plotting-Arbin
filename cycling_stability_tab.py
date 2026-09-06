f5cb37cc1aa53bd274aab7e513f0397a7374b7b4
arijitroy07 <ariex.blaster@gmail.com>
2026-09-06 13:29:32 -0500

    Update cycling_stability_tab.py

--- cycling_stability_tab.py
+++ cycling_stability_tab.py
@@ -409,11 +409,11 @@ def render(ui):
             )
 
 
-ax_top, ax_bottom = fig.axes[:2]
+            ax_top, ax_bottom = fig.axes[:2]
 
 # Exactly 1 minor tick between major ticks on BOTH Y axes
-ax_top.yaxis.set_minor_locator(AutoMinorLocator(2))
-ax_bottom.yaxis.set_minor_locator(AutoMinorLocator(2))
+                ax_top.yaxis.set_minor_locator(AutoMinorLocator(2))
+                ax_bottom.yaxis.set_minor_locator(AutoMinorLocator(2))
 
 
             buf = io.BytesIO()
